02a75ab0f1031bc69634597440a23aa69383758b
akhilkateja123 <akhilkateja@gmail.com>
2017-06-08 11:25:54 -0500

    More Code.

---
+++ CopyListWithRandomPointer.java
@@ -0,0 +1,42 @@
+package com.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class RandomListNode {
+	int label;
+	RandomListNode next, random;
+
+	RandomListNode(int x) {
+		this.label = x;
+	}
+};
+
+public class CopyListWithRandomPointer {
+	public RandomListNode copyRandomList(RandomListNode head) {
+		if (head == null)
+			return null;
+
+		Map<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+
+		RandomListNode curr = head;
+
+		while (curr != null) {
+			map.put(curr, new RandomListNode(curr.label));
+			curr = curr.next;
+		}
+
+		for (RandomListNode node : map.keySet()) {
+			RandomListNode currentNode = map.get(node);
+			currentNode.next = map.get(node.next);
+			currentNode.random = map.get(node.random);
+		}
+
+		return map.get(head);
+
+	}
+
+	public static void main(String... args) {
+
+	}
+}
