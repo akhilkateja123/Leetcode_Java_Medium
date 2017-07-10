d3678a09c32fbd9c7e480ace0db1bc8f40581c66
akhilkateja123 <akhilkateja@gmail.com>
2017-07-10 09:12:55 -0700

    Add files via upload
    More Code.

---
+++ ComplexNumberMultiplication.java
@@ -0,0 +1,31 @@
+package com.leetcode.medium;
+
+import java.util.StringTokenizer;
+
+public class ComplexNumberMultiplication {
+
+	public String complexNumberMultiply(String a, String b) {
+
+		StringTokenizer tokenizerA = new StringTokenizer(a, "+");
+		StringTokenizer tokenizerB = new StringTokenizer(b, "+");
+		
+		String complexTkn;
+		
+		int[] arrA = new int[] { Integer.parseInt(tokenizerA.nextToken()),
+				Integer.parseInt(tokenizerA.nextToken().replace("i","")) };
+		int[] arrB = new int[] { Integer.parseInt(tokenizerB.nextToken()),
+				Integer.parseInt(tokenizerB.nextToken().replace("i","")) };
+
+		int real = arrA[0] * arrB[0] + arrA[1] * arrB[1] * -1;
+		int complex = arrA[1] * arrB[0] + arrA[0] * arrB[1];
+
+		return real + "+" + complex + "i";
+
+	}
+
+	public static void main(String... args) {
+		ComplexNumberMultiplication obj = new ComplexNumberMultiplication();
+		System.out.println(obj.complexNumberMultiply("78+-76i", "-86+72i"));
+	}
+
+}
